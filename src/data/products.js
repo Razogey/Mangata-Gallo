ffe9a82897111dcc66a29efebc6a72be3ead6cfd
razogey <razogey88@gmail.com>
2026-09-01 20:38:16 +0300

    refactor: separate product data from card component

---
+++ src/data/products.js
@@ -0,0 +1,29 @@
+import engagement from "../assets/products/engagement.jpg";
+import wedding from "../assets/products/wedding.jpg";
+import collection from "../assets/products/collection.jpg";
+
+const products = [
+    {
+        image: engagement,
+        title: "Engagement Rings",
+        description:
+            "Find the perfect symbol of your love with our collection of elegant diamond engagement rings, thoughtfully designed for your special moment.",
+        link: "Discover Rings",
+    },
+    {
+        image: wedding,
+        title: "Wedding Collection",
+        description:
+            "Celebrate your journey together with beautifully crafted pieces designed to complement every unforgettable moment of your wedding day.",
+        link: "View Collection",
+    },
+    {
+        image: collection,
+        title: "Luxury Jewelry Collections",
+        description:
+            "Explore Mangata & Gallo's signature designs, where classic elegance meets exceptional craftsmanship and modern sophistication.",
+        link: "Shop Jewelry",
+    },
+];
+
+export default products;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,23 @@
-import Banner from '../components/Banner';
-import Card from '../components/Card';
-import engagement from "../assets/products/engagement.jpg";
-import wedding from "../assets/products/wedding.jpg";
-import collection from "../assets/products/collection.jpg";
-
+import Banner from "../components/Banner";
+import Card from "../components/Card";
+import products from "../data/products";
 
 export default function Home() {
     return (
         <>
             <Banner />
 
-            <section className='cards' id='collections'>
-                <Card
-                    image={engagement}
-                    title='Engagement Rings'
-                    description='Find the perfect symbol of your love with our collection of elegant diamond engagement rings,
-                thoughtfully designed for your special moment.'
-                    link='Discover Rings'
-                />
-
-                <Card
-                    image={wedding}
-                    title='Wedding Collection'
-                    description='Celebrate your journey together with beautifully crafted pieces designed to complement every
-                unforgettable moment of your wedding day'
-                    link='View Collection'
-                />
-
-                <Card 
-                    image={collection}
-                    title='Luxury Jewelry Collections'
-                    description="Explore Mangata & Gallo's signature designs, where classic elegance meets exceptional craftsmanship
-                and modern sophistication."
-                    link='Shop Jewelry'
-                />
+            <section className="cards" id="collections">
+                {products.map((product) => (
+                    <Card
+                        key={product.title}
+                        image={product.image}
+                        title={product.title}
+                        description={product.description}
+                        link={product.link}
+                    />
+                ))}
             </section>
         </>
-    )
+    );
 }
