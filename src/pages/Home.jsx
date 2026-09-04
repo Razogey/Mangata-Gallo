29b8e57c553b7d4b6e8f11982f4beb41f56f8242
razogey <razogey88@gmail.com>
2026-09-04 03:36:52 +0300

    feat: refactor Home component layout and enhance styling for collections section; improve responsiveness and accessibility

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import Banner from "../components/Banner";
 import Card from "../components/Card";
 import products from "../data/products";
-
 import BannerImg from "../assets/banner.jpg";
 
 export default function Home() {
     return (
         <main className="home-page">
-            <Banner 
+            <Banner
                 title="Timeless Jewelry for Life's Most Beautiful Moments"
                 description="Discover exquisite jewelry crafted with exceptional materials and timeless design, created to celebrate your most meaningful occasions."
                 image={BannerImg}
@@ -15,19 +14,27 @@ export default function Home() {
                 buttonPath="/collections"
             />
 
-            <section className="cards" id="collections" aria-label="Collections-title">
-                <h2 id="collections-title">Our Collections</h2>
-                {products.map((product) => (
-                    <Card
-                        key={product.id}
-                        image={product.image}
-                        title={product.title}
-                        description={product.description}
-                        link={product.link}
-                        path={product.path}
-                        className="product-card"
-                    />
-                ))}
+            <section
+                className="collections-section"
+                aria-labelledby="collections-title"
+            >
+                <h2 id="collections-title" className="collections-title">
+                    Our Collections
+                </h2>
+
+                <div className="cards" id="collections">
+                    {products.map((product) => (
+                        <Card
+                            key={product.id}
+                            image={product.image}
+                            title={product.title}
+                            description={product.description}
+                            link={product.link}
+                            path={product.path}
+                            className="product-card"
+                        />
+                    ))}
+                </div>
             </section>
         </main>
     );
