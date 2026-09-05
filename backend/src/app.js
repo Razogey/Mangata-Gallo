5b8b3143195e85b3f06c48b688eb45db67488a29
razogey <razogey88@gmail.com>
2026-09-05 05:40:35 +0300

    feat(database): implement Prisma client and add database connection test endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,29 @@
+import "dotenv/config";
 import express from 'express';
 import cors from 'cors';
+import prisma from './config/prisma.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/api/db-test", async (req, res) => {
+    try {
+        await prisma.$queryRaw`SELECT 1`;
+
+        res.json({
+            status: 'ok',
+            message: 'Database connection is working!'
+        })
+    } catch (error) {
+        console.error('Database connection error:', error);
+        res.status(500).json({
+            status: 'error',
+            message: 'Database connection failed!'
+        });
+    }
+})
 
 app.get('/api/health', (req, res) => {
     res.status(200).json({ status: 'ok', message: 'Mangata & Gallo API is running!' });
