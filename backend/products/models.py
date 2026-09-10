16ac1576cf7f0e645b9b72bda63a0cf630938881
razogey <razogey88@gmail.com>
2026-09-10 22:19:00 +0300

    feat: add initial migration and model definition for Product in products app

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    category = models.ForeignKey(
+        "categories.Category",
+        on_delete=models.PROTECT,
+        related_name="products",
+    )
+
+    collection = models.ForeignKey(
+        "product_collections.Collection",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="products",
+    )
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "products"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
