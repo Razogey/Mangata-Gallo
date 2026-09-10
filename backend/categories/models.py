34d4b750c0f9fb9d4d0f5e11c3576dd9c5d1d549
razogey <razogey88@gmail.com>
2026-09-10 22:17:18 +0300

    feat: add initial migration and model definition for Category in categories app

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    slug = models.SlugField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "categories"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
