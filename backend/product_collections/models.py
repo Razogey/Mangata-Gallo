d45ab94c5c033ee934d21b470fa902b5428637b9
razogey <razogey88@gmail.com>
2026-09-10 22:17:03 +0300

    feat: add initial migration and model definition for Collection in product_collections app

--- backend/product_collections/models.py
+++ backend/product_collections/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Collection(models.Model):
+    slug = models.SlugField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    details = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "collections"
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
