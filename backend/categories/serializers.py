572f37027e570b87fc3bba00345415a5256d8735
razogey <razogey88@gmail.com>
2026-09-10 22:47:17 +0300

    feat: add Category API with serializers, views, and routing

---
+++ backend/categories/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from .models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            "id",
+            "slug",
+            "name",
+            "description",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = [
+            "id",
+            "created_at",
+            "updated_at",
+        ]
--- backend/categories/views.py
+++ backend/categories/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from accounts.permissions import IsStaffOrAdmin
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [AllowAny()]
+
+        return [IsStaffOrAdmin()]
