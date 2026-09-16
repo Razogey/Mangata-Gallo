db6961f3fc75ddb99b06e0cb79083edc090b7b9d
razogey <razogey88@gmail.com>
2026-09-16 07:18:04 +0300

    feat: add CORS support and environment configuration for frontend API

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "categories",
     "product_collections",
     "rest_framework",
+    "corsheaders",
     "drf_spectacular",
     "rest_framework_simplejwt.token_blacklist",
     'django.contrib.admin',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +62,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = 'config.urls'
